b63a3f3cfef348087f07d6791268ca33701b52c4
mahdi-mhdn <mhdn1380@gmail.com>
2022-09-11 21:16:36 +0430

    removing short samples

--- dataset/4_Final_dataset.py
+++ dataset/4_Final_dataset.py
@@ -4,5 +4,6 @@ df1=pd.read_csv('chats.csv')
 df2=pd.read_csv('insults.csv')
 
 df=pd.concat([df1,df2],ignore_index=True)
+df=df.drop(df[df['text'].str.len()<2].index)
 df = df.sample(frac = 1,ignore_index=True)
 df.to_csv('data.csv', index=False)
